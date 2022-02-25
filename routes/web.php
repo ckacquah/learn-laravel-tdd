327f49c574b3d3ffed5776859fa7c5c4ab1f2fc4
ckacquah <ckacquah001@gmail.com>
2022-02-25 09:09:50 +0000

    installing laravel auth & ui

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,10 @@
 <?php
 
 use App\Http\Controllers\AuthorController;
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BooksController;
+use App\Http\Controllers\CheckoutBookController;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,10 +15,15 @@ use App\Http\Controllers\BooksController;
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+ */
 
 Route::post('/books', [BooksController::class, 'store']);
 Route::patch('/books/{book}/{date}/{slug}', [BooksController::class, 'update']);
 Route::delete('/books/{book}/{date}/{slug}', [BooksController::class, 'destroy']);
 
 Route::post('/authors', [AuthorController::class, 'store']);
+Route::post('/checkout/{book}', [CheckoutBookController::class, 'checkout']);
+Route::post('/checkin/{book}', [CheckoutBookController::class, 'checkin']);
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
